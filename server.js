8508a59dedcad139cd0e63706b144e7869440f9a
femi-connect <60244053+femiconnect@users.noreply.github.com>
2021-10-06 02:25:27 +0100

    enabled cors

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
+   res.set('Access-Control-Allow-Origin', '*');
    res.send('<h1>Femiconnect says hi...</h1>');
 });
 
 app.get('/testimonials', (req, res) => {
+   res.set('Access-Control-Allow-Origin', '*');
    res.send(testimonialData);
 });
 
